b0e6dd45b562525ef31a3997f77e6cf2473efc9a
Victor Ferreira <victor.santos@oi.net.br>
2018-07-13 15:15:45 -0300

    Conexça com banco de dados

--- index.php
+++ index.php
@@ -5,4 +5,19 @@ echo '<br/><br/>';
 echo json_encode($_SERVER);
 echo '<br/><br/>';
 echo $_SERVER['SERVER_ADDR'];
+
+$conn = mysqli_connect('mysql','openshift123','openshift123','workshop');
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$result = $conn->query("SELECT nome FROM cidade");
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<h3>" . $row["nome"] . "</h3>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
